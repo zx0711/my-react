fe4ae00c78ead92260e3f55144cd90cb9c74a06e
xiao.zhang <xiao.zhang@ximalaya.com>
2020-10-09 14:37:18 +0800

    10.9复习笔记

--- src/page/01.函数式组建用法.jsx
+++ src/page/01.函数式组建用法.jsx
@@ -1,19 +1,11 @@
-<Form className="three-distribute">
-          {showData.map((item, index) => {
-            return (
-              <Form.Item
-                {...formItemLayout}
-                label={item.label}
-                required
-                key={index}
-              >
-                {item.content}
-              </Form.Item>
-            )
-          })}
+;<Form className="three-distribute">
+  {showData.map((item, index) => {
+    return (
+      <Form.Item {...formItemLayout} label={item.label} required key={index}>
+        {item.content}
+      </Form.Item>
+    )
+  })}
 </Form>
 
 // 上面这种就是箭头函数，就是我平常常常用到的，叫函数作为子组建或者是函数式组建
-
-
-
