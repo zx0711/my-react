5615f8bd52a532ce2297d6351595fa34c58f6b19
xiao.zhang <xiao.zhang@ximalaya.com>
2020-10-27 19:38:52 +0800

    add jsx转为虚拟dom（js对象）源码解析

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@
  * @Author: xiao.zhang
  * @Date: 2019-09-19 14:50:16
  * @LastEditors: xiao.zhang
- * @LastEditTime: 2020-10-26 22:24:37
+ * @LastEditTime: 2020-10-27 19:37:46
  */
 import React from 'react'
 // import { Provider } from 'mobx-react'
 // import store from './mobxStore/index'
 // import ContextExample from './react小书/12.contextExample'
 // import ContextExample from './react小书/14.newContext'
-import ReactVirDom from './page/22.react+虚拟dom'
 // import './App.css'
 
 // 函数式组建
@@ -21,7 +20,6 @@ function App() {
       {/* 把mobx整体注入到react中去 */}
       {/* <Provider store={store}></Provider> */}
       {/* <ContextExample></ContextExample> */}
-      <ReactVirDom></ReactVirDom>
     </div>
   )
 }
